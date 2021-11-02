4314fc2446c6aa42a52b4746fdf628f8b677b9e6
Hakon Mar Jonsson <hakonmarjonsson@Hakons-MacBook-Pro.local>
2021-11-02 11:06:00 +0000

    payment progress

---
+++ PaymentService/PaymentService.py
@@ -0,0 +1,38 @@
+def luhn_check(number):
+    if number.isdigit():
+        last_digit = int(str(number)[-1])
+        reverse_sequence = list(int(d) for d in str(int(number[-2::-1])))
+
+        for i in range(0, len(reverse_sequence), 2):
+            reverse_sequence[i] *= 2
+
+        for i in range(len(reverse_sequence)):
+            if reverse_sequence[i] > 9:
+                reverse_sequence[i] -= 9
+
+        sum_of_digits = 0
+        for i in range(len(reverse_sequence)):
+            sum_of_digits += reverse_sequence[i]
+
+        result = divmod(sum_of_digits, 10)
+
+        if result != last_digit:
+            return False
+        return True
+
+    return False
+
+def month_check(number):
+    if number<13 and number>0:
+        return True
+    return False
+
+def year_check(number):
+    if len(number)==4 and number.isdigit():
+        return True
+    return False
+
+def cvc_check(number):
+    if len(number)==3 and number.isdigit():
+        return True
+    return False
